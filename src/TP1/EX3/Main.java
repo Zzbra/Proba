56704863a620331c87a58f6eed6b0f866b421a64
Etienne <roux.etienne141@gmail.com>
2021-02-03 18:31:36 +0100

    dynamic drawing

--- src/TP1/EX3/Main.java
+++ src/TP1/EX3/Main.java
@@ -1,36 +1,35 @@
 package TP1.EX3;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        double valeurMax = 10;
+        double valeurMax = 1;
         double valeursX[] = {0.25 * valeurMax, 0.25 * valeurMax, 0.75 * valeurMax, 0.75 * valeurMax};
         double valeursY[] = {0.25 * valeurMax, 0.75 * valeurMax, 0.75 * valeurMax, 0.25 * valeurMax};
-        double valeursX2[] = {2.5, 2.5, 7.5, 7.5};
-        double valeursY2[] = {2.5, 7.5, 7.5, 2.5};
 
-//        PolygoneListe polygoneListe = new PolygoneListe(valeursX, valeursY, valeurMax);
-        PolygoneListe polygoneListe = SimplePolygonGenerator.genetatePolygon(50,1);
-        DrawPolygon.draw(polygoneListe);
+        PolygoneListe polygoneListe = new PolygoneListe(valeursX, valeursY, valeurMax);
+//        PolygoneListe polygoneListe = SimplePolygonGenerator.genetatePolygon(50,1);
+        Drawer drawer = new Drawer();
+        drawer.draw(polygoneListe);
 
-//        System.out.println(polygoneListe.contient(new Point2D(0, 0)));
-//        System.out.println(polygoneListe.contient(new Point2D(2.5, 5)));
-//        System.out.println(polygoneListe.contient(new Point2D(5, 5)));
-//        System.out.println(polygoneListe.contient(new Point2D(9, 1)));
-
-        System.out.println(monteCarlo(polygoneListe, valeurMax, 10_000_000));
-        System.out.println(formuleEnLacet(polygoneListe));
+        System.out.println("Monte Carlo: " + monteCarlo(polygoneListe, valeurMax, 10_000_000, drawer));
+        System.out.println("Lacet: " + formuleEnLacet(polygoneListe));
     }
 
-    private static double monteCarlo(PolygoneListe polygoneListe, double valeurMax, double nbIterations){
+    private static double monteCarlo(PolygoneListe polygoneListe, double valeurMax, double nbIterations, Drawer drawer){
         double c = 0;
         for (int i = 0; i < nbIterations; i++) {
             Point2D point = new Point2D(Math.random() * valeurMax, Math.random() * valeurMax);
+            Color color = Color.RED;
             if(polygoneListe.contient(point)){
                 c++;
+                color = Color.GREEN;
             }
+            drawer.draw(point, color);
         }
 
-        return 10*c*valeurMax/nbIterations;
+        return c*valeurMax/nbIterations;
     }
 
     private static double formuleEnLacet(PolygoneListe polygoneListe){
@@ -41,7 +40,7 @@ public class Main {
         for (int i = 0; i < polygoneListe.size(); i++) {
             somme -= polygoneListe.getSommet(i).getY() * polygoneListe.getSommet(i+1).getX();
         }
-        // Ici, sans le *-1 (-2.0) on obtient -(l'aire). C'est surement du au sens de rotation de l'ordre des sommets
-        return somme/2.0;
+        // Valeur absolue car le signe dépend du sens de rotation de l'ordre des sommets
+        return Math.abs(somme/2.0);
     }
 }
